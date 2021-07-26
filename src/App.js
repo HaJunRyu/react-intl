20fe221602a7baa4cb2f7367768533fcb8e572dd
HaJunRyu <hjr4557@naver.com>
2021-07-26 17:36:39 +0900

    Feat: 국가코드를 상태로 관리하고 해당 상태가 변경되는것에 맞춰 react-intl이 렌더링하는 json파일 교체
    Changes to be committed:
    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { FormattedMessage, IntlProvider } from 'react-intl';
+import { useState } from 'react';
+import locale from './locale';
 
 function App() {
+  const [lang, setLang] = useState('en');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale="en" messages={locale[lang]} defaultLocale="en">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <FormattedMessage id="reactMessage" />
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button
+            onClick={() => {
+              setLang('en');
+            }}
+          >
+            English
+          </button>
+          <button
+            onClick={() => {
+              setLang('kr');
+            }}
+          >
+            한국어
+          </button>
+          <button
+            onClick={() => {
+              setLang('ch');
+            }}
+          >
+            中国人
+          </button>
+          <button
+            onClick={() => {
+              setLang('be');
+            }}
+          >
+            Tiếng Việt
+          </button>
+        </header>
+      </div>
+    </IntlProvider>
   );
 }
 
