d75859fb3b86060073c90045ce5b024b781128ae
HaJunRyu <hjr4557@naver.com>
2021-07-26 17:27:44 +0900

    Feat: 언어별 메시지 설정 및 모듈화
    Changes to be committed:
    new file:   src/locale/be.json
    new file:   src/locale/ch.json
    new file:   src/locale/en.json
    new file:   src/locale/index.js
    new file:   src/locale/kr.json

---
+++ src/locale/index.js
@@ -0,0 +1,13 @@
+import kr from './kr.json';
+import en from './en.json';
+import ch from './ch.json';
+import be from './be.json';
+
+const locale = {
+  kr,
+  en,
+  ch,
+  be,
+};
+
+export default locale;
